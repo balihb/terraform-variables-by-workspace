630be768a7b98e5b56abd6f90259e98e513492c6
Hamorszky, Balazs <balihb@gmail.com>
2020-12-29 16:24:28 +0100

    added check, license update, reformat

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 locals {
-  default_variables_path  = "${path.cwd}/defaults.yaml"
+  default_variables_path   = "${path.cwd}/defaults.yaml"
   workspace_variables_path = "${path.cwd}/workspaces/${terraform.workspace}.yaml"
 
   default_variables = yamldecode(fileexists(local.default_variables_path) ?
     file(local.default_variables_path) : yamlencode({})
   )
   workspace_variables = yamldecode(fileexists(local.workspace_variables_path) ?
-    file(local.workspace_variables_path): yamlencode({})
+    file(local.workspace_variables_path) : yamlencode({})
   )
 
   merged_variables = merge(
